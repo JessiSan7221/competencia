4907d96f225c1b791669114e8550ce44a57d70fd
jessisanabria20@gmail.com <jessisanabria20@gmail.com>
2026-03-19 21:09:39 -0500

    reto 1

---
+++ reto1/ContadorApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Contador contador = new Contador();
+
+Console.WriteLine(contador.Incrementar());
+Console.WriteLine(contador.Incrementar());
+Console.WriteLine(contador.Decrementar());
+Console.WriteLine(contador.Decrementar());
